3f19fa3cdd2f581311e0d58cbc5512ba7b2e5197
alok <alok.sharma.17@mountblue.tech>
2021-10-08 16:40:29 +0530

    adding Nav element

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import './App.css';
 import HomeScreen from './HomeScreen';
+import Nav from './Nav';
 
 function App() {
   return (
     <div className="app">
       <HomeScreen />
+      <Nav/>
     </div>
   );
 }
