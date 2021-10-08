fd9188152152a8db1efd834a5034d4a170b80da8
Abhishek Yadav <abhishek.yadav.17@mountblue.tech>
2021-10-08 12:14:40 +0530

    correcting folder structure and configure firebase

---
+++ src/firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyAqu6UNEruUye0XTkd92xc27LJYr83ubmc",
+  authDomain: "netflix-clone-a8f58.firebaseapp.com",
+  projectId: "netflix-clone-a8f58",
+  storageBucket: "netflix-clone-a8f58.appspot.com",
+  messagingSenderId: "624775541514",
+  appId: "1:624775541514:web:c460f314922b65a82e40f3"
+};
