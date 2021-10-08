93decd8b10eb87ba5b9fa3c97be4b36401eab8bf
alok <alok.sharma.17@mountblue.tech>
2021-10-08 17:13:42 +0530

    modified component

--- src/App.js
+++ src/App.js
@@ -1,13 +1,12 @@
 import React from 'react';
 import './App.css';
 import HomeScreen from './HomeScreen';
-import Nav from './Nav';
+
 
 function App() {
   return (
     <div className="app">
       <HomeScreen />
-      <Nav/>
     </div>
   );
 }
