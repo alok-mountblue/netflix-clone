f9f04a31fb936fa8a9108994db0ef6c4955b8276
alok <alok.sharma.17@mountblue.tech>
2021-10-09 15:58:38 +0530

    update src/Row.js

--- src/Row.js
+++ src/Row.js
@@ -1,41 +1,43 @@
-import React, { useEffect, useState } from 'react';
-import './Row.css';
+
+import React from 'react';
+import "./Row.css";
 import axios from './axios';
 
-function Row({title, fetchUrl, isLargeRow = false}) {
-    const [movies, setMovies] = useState([]);
-
-    const base_url = 'https://image.tmdb.org/t/p/original';
-
-    useEffect(() => {
-        async function fetchData() {
-        const request = await axios.get(fetchUrl);
-        setMovies(request.data.results);
-        return request;
+class Row extends React.Component{
+    constructor(props){
+        super(props);
+        this.state={
+            movies:[]
         }
-
-        fetchData();
-    }, [fetchUrl]);
+    }
+    async componentDidMount(){
+        const request = await axios.get(this.props.fetchUrl);
+        this.setState({movies:request.data.results});
+    }
+  render(){
     return (
+
         <div className='row'>
-            <h2>{title}</h2>
-            <div className='row_posters'>   
-                {movies.map((movie) =>
-                    ((isLargeRow && movie.poster_path) ||
-                    (!isLargeRow && movie.backdrop_path)) && (
+            <h2>{this.props.title}</h2>
+            <div className='row_posters'> 
+                {this.state.movies.map((movie) =>
+                    ((this.props.isLargeRow && movie.poster_path) ||
+                    (!this.props.isLargeRow && movie.backdrop_path)) && (
                     <img
-                        className={`row_poster ${isLargeRow && 'row_posterLarge'}`}
+                        className={`row_poster ${this.props.isLargeRow && 'row_posterLarge'}`}
                         key={movie.id}
-                        src={`${base_url}${
-                        isLargeRow ? movie.poster_path : movie.backdrop_path
+                        src={`https://image.tmdb.org/t/p/original${
+                        this.props.isLargeRow ? movie.poster_path : movie.backdrop_path
                         }`}
-                        alt={movie.name}
-                    />
+                       alt={movie.name}
+                   />
                     )
                 )}
             </div>
-        </div>
+            </div>
     );
+ }
+ 
 }
 
 export default Row;
