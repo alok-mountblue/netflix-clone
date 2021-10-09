1d573bbed007be1715cbb29303d0713e77a324f9
Sutirtha Dey <sutirtha.dey.17@mountblue.tech>
2021-10-09 18:20:29 +0530

    added the ProfileScreen Component

--- src/App.js
+++ src/App.js
@@ -1,14 +1,18 @@
 import React from 'react';
 import './App.css';
 import HomeScreen from './screens/HomeScreen';
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import ProfileScreen from './screens/ProfileScreen';
 
 function App() {
   return (
-    <div className="app">  
+    <div className='app'>
       <Router>
         <Switch>
-          <Route exact path="/">
+          <Route path='/profile'>
+            <ProfileScreen />
+          </Route>
+          <Route exact path='/'>
             <HomeScreen />
           </Route>
         </Switch>
