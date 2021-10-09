8140d567cc9c5868f4d454884058fd34b1b71c6d
Sutirtha Dey <sutirtha.dey.17@mountblue.tech>
2021-10-09 18:22:41 +0530

    re-implemented using class-based components

--- src/screens/ProfileScreen.js
+++ src/screens/ProfileScreen.js
@@ -1,36 +1,37 @@
 import React from 'react';
-import Nav from './Nav';
+import Nav from '../Nav';
 import './ProfileScreen.css';
 
-export const ProfileScreen = () => {
+class ProfileScreen extends React.Component {
   // const user = useSelector(selectUser);
-  return (
-    <div className='profileScreen'>
-      <Nav />
-      <div className='ProfileScreen_body'>
-        <h1>Edit Profile</h1>
-        <div className='profileScreen_info'>
-          <img
-            src='https://pbs.twing.com/profile_images/1240119990411550720/hBEe3tdn_400x400.png'
-            alt=''
-          />
-          <div className='profileScreen_details'>
-            {/* <h2>{user.email}</h2> */}
-            <div className='profileScreen_plans'>
-              <h3>Plans</h3>
-              <p></p>
-              <button
-                // onClick={() => auth.signOut}
-                className='ProfileScreen_signOut'
-              >
-                Sign Out
-              </button>
+  render() {
+    return (
+      <div className='profileScreen'>
+        <Nav />
+        <div className='profileScreen_body'>
+          <h1>Edit Profile</h1>
+          <div className='profileScreen_info'>
+            <img
+              src='https://pbs.twimg.com/profile_images/1240119990411550720/hBEe3tdn_400x400.png'
+              alt=''
+            />
+            <div className='profileScreen_details'>
+              {/* <h2>{user.email}</h2> */}
+              <div className='profileScreen_plans'>
+                <h3>Plans</h3>
+                <button
+                  // onClick={() => auth.signOut}
+                  className='profileScreen_signOut'
+                >
+                  Sign Out
+                </button>
+              </div>
             </div>
           </div>
         </div>
       </div>
-    </div>
-  );
-};
+    );
+  }
+}
 
 export default ProfileScreen;
