950f65405d68f862eeb31757d5bdd0cf9b34de46
Abhishek Yadav <abhishek.yadav.17@mountblue.tech>
2021-10-11 13:22:01 +0530

    Adding login and logout feature

--- src/screens/ProfileScreen.js
+++ src/screens/ProfileScreen.js
@@ -1,27 +1,30 @@
 import React from 'react';
-import Nav from './Nav';
+import { useSelector } from 'react-redux';
+import { selectUser } from '../features/userSlice';
+import { auth } from '../firebase';
+import Nav from '../Nav';
 import './ProfileScreen.css';
 
 export const ProfileScreen = () => {
-  // const user = useSelector(selectUser);
+  const user = useSelector(selectUser);
   return (
     <div className='profileScreen'>
       <Nav />
-      <div className='ProfileScreen_body'>
+      <div className='profileScreen_body'>
         <h1>Edit Profile</h1>
         <div className='profileScreen_info'>
           <img
-            src='https://pbs.twing.com/profile_images/1240119990411550720/hBEe3tdn_400x400.png'
+            src='https://pbs.twimg.com/profile_images/1240119990411550720/hBEe3tdn_400x400.png'
             alt=''
           />
           <div className='profileScreen_details'>
-            {/* <h2>{user.email}</h2> */}
+            <h2>{user.email}</h2>
             <div className='profileScreen_plans'>
               <h3>Plans</h3>
               <p></p>
               <button
-                // onClick={() => auth.signOut}
-                className='ProfileScreen_signOut'
+                onClick={() => auth.signOut()}
+                className='profileScreen_signOut'
               >
                 Sign Out
               </button>
