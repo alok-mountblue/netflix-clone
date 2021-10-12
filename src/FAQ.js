d8e1a13d580079f3c5d464860a6d8ecf1268032e
Abhishek Yadav <abhishek.yadav.17@mountblue.tech>
2021-10-12 15:45:31 +0530

    Fixing FAQ

--- src/FAQ.js
+++ src/FAQ.js
@@ -70,7 +70,7 @@ class Faq extends React.Component {
           <p>
             Netflix is a streaming service that offers a wide variety of
             award-winning TV shows, movies, anime, documentaries and more – on
-            thousands of internet-connected devices. You can watch as much as
+            thousands of internet-connected devices. <br/><br/>You can watch as much as
             you want, whenever you want, without a single ad – all for one low
             monthly price. There's always something new to discover, and new TV
             shows and movies are added every week!
--- src/LoginScreen.js
+++ src/LoginScreen.js
@@ -10,48 +10,50 @@ class LoginScreen extends React.Component {
   }
   render() {
     return (
-      <div className="loginScreen">
-        <div className="loginScreen_background">
-          <img
-            className="loginScreen_logo"
-            src="https://assets.stickpng.com/images/580b57fcd9996e24bc43c529.png"
-            alt=""
-          />
-          <button
-            className="loginScreen_button"
-            onClick={() => this.setState({ signIn: true })}
-          >
-            Sign In
-          </button>
-          <div className="loginScreen_gradient" />
-        </div>
-        <div className="loginScreen_body">
-          {this.state.signIn ? (
-            <SignUpScreen />
-          ) : (
-            <>
-              <h1>Unlimited films, TV programs and more.</h1>
-              <h2>Watch anywhere. Cancel at any time.</h2>
-              <h3>
-                Ready to watch? Enter your email to create or restart your
-                membership
-              </h3>
-              <div className="loginScreen_input">
-                <form>
-                  <input type="email" placeholder="Email Address" />
-                  <button
-                    onClick={() => this.setState({ signIn: true })}
-                    className="loginScreen_getStarted"
-                  >
-                    GET STARTED
-                  </button>
-                </form>
-              </div>
-            </>
-          )}
+      <>
+        <div className="loginScreen">
+          <div className="loginScreen_background">
+            <img
+              className="loginScreen_logo"
+              src="https://assets.stickpng.com/images/580b57fcd9996e24bc43c529.png"
+              alt=""
+            />
+            <button
+              className="loginScreen_button"
+              onClick={() => this.setState({ signIn: true })}
+            >
+              Sign In
+            </button>
+            <div className="loginScreen_gradient" />
+          </div>
+          <div className="loginScreen_body">
+            {this.state.signIn ? (
+              <SignUpScreen />
+            ) : (
+              <>
+                <h1>Unlimited films, TV programs and more.</h1>
+                <h2>Watch anywhere. Cancel at any time.</h2>
+                <h3>
+                  Ready to watch? Enter your email to create or restart your
+                  membership
+                </h3>
+                <div className="loginScreen_input">
+                  <form>
+                    <input type="email" placeholder="Email Address" />
+                    <button
+                      onClick={() => this.setState({ signIn: true })}
+                      className="loginScreen_getStarted"
+                    >
+                      GET STARTED
+                    </button>
+                  </form>
+                </div>
+              </>
+            )}
+          </div>
         </div>
         <Faq />
-      </div>
+      </>
     );
   }
 }
