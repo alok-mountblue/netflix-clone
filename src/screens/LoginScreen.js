5ed0fc251859efd66c1c565479fcaec016c56b73
Abhishek Yadav <abhishek.yadav.17@mountblue.tech>
2021-10-12 22:14:48 +0530

    Added FAQ page

--- src/screens/LoginScreen.js
+++ src/screens/LoginScreen.js
@@ -2,6 +2,7 @@ import React from 'react';
 import './LoginScreen.css';
 import Footer from '../footer';
 import SignInScreen from './SignInScreen';
+import Faq from '../FAQ';
 
 class LoginScreen extends React.Component {
   constructor() {
@@ -10,47 +11,50 @@ class LoginScreen extends React.Component {
   }
   render() {
     return (
-      <div className="loginScreen">
-        <div className="loginScreen_background">
-          <img
-            className="loginScreen_logo"
-            src="https://assets.stickpng.com/images/580b57fcd9996e24bc43c529.png"
-            alt=""
-          />
-          <button
-            className="loginScreen_button"
-            onClick={() => this.setState({ signIn: true })}
-          >
-            Sign In
-          </button>
-          <div className="loginScreen_gradient" />
+      <>
+        <div className="loginScreen">
+          <div className="loginScreen_background">
+            <img
+              className="loginScreen_logo"
+              src="https://assets.stickpng.com/images/580b57fcd9996e24bc43c529.png"
+              alt=""
+            />
+            <button
+              className="loginScreen_button"
+              onClick={() => this.setState({ signIn: true })}
+            >
+              Sign In
+            </button>
+            <div className="loginScreen_gradient" />
+          </div>
+          <div className="loginScreen_body">
+            {this.state.signIn ? (
+              <SignInScreen />
+            ) : (
+              <>
+                <h1>Unlimited films, TV programs and more.</h1>
+                <h2>Watch anywhere. Cancel at any time.</h2>
+                <h3>
+                  Ready to watch? Enter your email to create or restart your
+                  membership
+                </h3>
+                <div className="loginScreen_input">
+                  <form>
+                    <button
+                      onClick={() => this.setState({ signIn: true })}
+                      className="loginScreen_getStarted"
+                    >
+                      GET STARTED
+                    </button>
+                  </form>
+                </div>
+              </>
+            )}
+          </div>
         </div>
-        <div className="loginScreen_body">
-          {this.state.signIn ? (
-            <SignInScreen />
-          ) : (
-            <>
-              <h1>Unlimited films, TV programs and more.</h1>
-              <h2>Watch anywhere. Cancel at any time.</h2>
-              <h3>
-                Ready to watch? Enter your email to create or restart your
-                membership
-              </h3>
-              <div className="loginScreen_input">
-                <form>
-                  <button
-                    onClick={() => this.setState({ signIn: true })}
-                    className="loginScreen_getStarted"
-                  >
-                    GET STARTED
-                  </button>
-                </form>
-              </div>
-            </>
-          )}
-        </div>
+        <Faq />
         <Footer/>
-      </div>
+      </>
     );
   }
 }
