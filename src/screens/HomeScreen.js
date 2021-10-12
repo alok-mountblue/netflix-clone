9b86d07b89921e48d2eec96365f37a9f7a8238df
alok <alok.sharma.17@mountblue.tech>
2021-10-12 22:45:55 +0530

    update HomeScreen.js

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -15,34 +15,42 @@ const HomeScreen = () => (
             <Row 
                 title="NETFLIX ORIGINALS"
                 fetchUrl={requests.fetchNetflixOriginals}
+                id="NO"
                 isLargeRow
             />
             <Row 
                 title="Trending Now"
+                id="TN"
                 fetchUrl={requests.fetchTrending}
             />
             <Row 
                 title="Top Rated"
+                id="TR"
                 fetchUrl={requests.fetchTopRated}
             />
             <Row 
                 title="Action Movies"
+                id="AM"
                 fetchUrl={requests.fetchActionMovies}
             />
             <Row 
                 title="Comedy Movies"
+                id="CM"
                 fetchUrl={requests.fetchComedyMovies}
             />
             <Row 
                 title="Horror Movies"
+                id="HM"
                 fetchUrl={requests.fetchHorrorMovies}
             />
             <Row 
                 title="Romance Movies"
+                id="RM"
                 fetchUrl={requests.fetchRomanceMovies}
             />
             <Row 
                 title="Documenteries"
+                id="DM"
                 fetchUrl={requests.fetchDocumentaries}
             />
         </div>
