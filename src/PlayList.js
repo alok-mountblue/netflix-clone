1b0ea1d5fe1f3da4fe813e2693de1ef87ccab71b
VASANTHAKUMAR B <vasanthakumar.b.17@mountblue.tech>
2021-10-13 11:49:08 +0530

    Modified playlist rows

--- src/PlayList.js
+++ src/PlayList.js
@@ -1,33 +1,39 @@
 import React from 'react';
 import './PlayList.css';
+import Nav from './Nav';
 
 class PlayList extends React.Component {
   render() {
     return (
-      <section style={{ backgroundColor: 'white' }}>
-        <h1>My Lists</h1>
-        {this.props.movieList.length === 0 ? (
-          <div>Watch list is empty...</div>
-        ) : (
-          this.props.movieList.map((movie) => (
-            <div>
-              <img
-                className="row_poster"
-                key={movie.id}
-                src={`https://image.tmdb.org/t/p/original${
-                  this.props.isLargeRow
-                    ? movie.poster_path
-                    : movie.backdrop_path
-                }`}
-                alt={movie.name}
-              />
-              <h3>{movie.name || movie.original_title}</h3>
-              <button onClick={() => this.props.removeFromPlayList(movie)}>
-                Remove
-              </button>
-            </div>
-          ))
-        )}
+      <section className="playlist-container">
+        <Nav />
+        <section className="playList">
+          <h1>My Lists</h1>
+          <main className="movieList">
+            {this.props.movieList.length === 0 ? (
+              <div className="playlist-empty">Watch list is empty...</div>
+            ) : (
+              this.props.movieList.map((movie) => (
+                <div>
+                  <img
+                    className="row_poster"
+                    key={movie.id}
+                    src={`https://image.tmdb.org/t/p/original${
+                      this.props.isLargeRow
+                        ? movie.poster_path
+                        : movie.backdrop_path
+                    }`}
+                    alt={movie.name}
+                  />
+                  <h3>{movie.name || movie.original_title}</h3>
+                  <button onClick={() => this.props.removeFromPlayList(movie)}>
+                    Remove
+                  </button>
+                </div>
+              ))
+            )}
+          </main>
+        </section>
       </section>
     );
   }
