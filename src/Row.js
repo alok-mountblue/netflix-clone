22997d41a9026c55bbc97c51ccedf5cf37f0bfd5
alok <alok.sharma.17@mountblue.tech>
2021-10-14 15:49:19 +0530

    modified Row.js

--- src/Row.js
+++ src/Row.js
@@ -2,6 +2,8 @@ import React from 'react'
 import axios from "./axios";
 import "./Row.css";
 import MovieModal from './movieTrailer';
+import ArrowForwardIosIcon from '@material-ui/icons/ArrowForwardIos';
+import ArrowBackIosIcon from '@material-ui/icons/ArrowBackIos';
 
 class Row extends React.Component{
     constructor(props){
@@ -32,18 +34,24 @@ class Row extends React.Component{
             <div className="row">
             {/** TITLE */}
             <h2>{this.props.title}</h2>
+            <div class="slider">
+            <div className="slider__arrow-left" ><span className="arrow" onClick={()=>{document.getElementById(this.props.id).scrollLeft-=(window.innerWidth-80)}}><ArrowBackIosIcon/></span></div>
                 <div id={this.props.id} className="row__posters">
                     {/**SEVERAL ROW__POSTER */}
                     {this.state.movies.map(movie=>(
-                        <img
+                           <img
                             key={movie.id}
                             onClick={() => this.handleClick(movie)}
                             className={`row__poster ${this.props.isLargeRow && "row__posterLarge"}`} 
                             src={`https://image.tmdb.org/t/p/original/${this.props.isLargeRow ? movie.poster_path : movie.backdrop_path}`} 
                             loading="lazy"
-                            alt={movie.name}/>
+                            text={movie.name}
+                            alt={movie.name}
+    
+                            />
                     ))}
-                    
+                    </div>
+                    <div className="slider__arrow-right" ><span className="arrow" onClick={()=>{document.getElementById(this.props.id).scrollLeft+=(window.innerWidth-80)}}><ArrowForwardIosIcon/></span></div>
                 </div>
     
             {this.state.modalVisibility && <MovieModal {...this.state.movieSelected} setModalVisibility={this.setModalVisibility}/>}
