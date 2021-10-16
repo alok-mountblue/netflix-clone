4d7deb4d66d4d16945f2d29db046b324f7f28cc9
alok <alok.sharma.17@mountblue.tech>
2021-10-16 09:45:37 +0530

    fixing bug

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -8,13 +8,6 @@ import Row from '../Row';
 
 
   class HomeScreen extends React.Component{
-    constructor(props){
-      super(props);
-      const {movieList,playList} = this.props;
-      this.state={
-        movieList,playList
-      }
-    }
 
     render(){
      return (
@@ -28,57 +21,57 @@ import Row from '../Row';
       fetchUrl={requests.fetchNetflixOriginals}
       id="NO"
       isLargeRow
-      playList={this.state.playList}
-      movieList={this.state.movieList}
+      playList={this.props.playList}
+      movieList={this.props.movieList}
     />
     <Row
       title="Trending Now"
       id="TN"
       fetchUrl={requests.fetchTrending}
-      playList={this.state.playList}
-      movieList={this.state.movieList}
+      playList={this.props.playList}
+      movieList={this.props.movieList}
     />
     <Row
       title="Top Rated"
       id="TR"
       fetchUrl={requests.fetchTopRated}
-      playList={this.state.playList}
-      movieList={this.state.movieList}
+      playList={this.props.playList}
+      movieList={this.props.movieList}
     />
     <Row
       title="Action Movies"
       id="AM"
       fetchUrl={requests.fetchActionMovies}
-      playList={this.state.playList}
-      movieList={this.state.movieList}
+      playList={this.props.playList}
+      movieList={this.props.movieList}
     />
     <Row
       title="Comedy Movies"
       id="CM"
       fetchUrl={requests.fetchComedyMovies}
-      playList={this.state.playList}
-      movieList={this.state.movieList}
+      playList={this.props.playList}
+      movieList={this.props.movieList}
     />
     <Row
       title="Horror Movies"
       id="HM"
       fetchUrl={requests.fetchHorrorMovies}
-      playList={this.state.playList}
-      movieList={this.state.movieList}
+      playList={this.props.playList}
+      movieList={this.props.movieList}
     />
     <Row
       title="Romance Movies"
       id="RM"
       fetchUrl={requests.fetchRomanceMovies}
-      playList={this.state.playList}
-      movieList={this.state.movieList}
+      playList={this.props.playList}
+      movieList={this.props.movieList}
     />
     <Row
       title="Documenteries"
       id="DM"
       fetchUrl={requests.fetchDocumentaries}
-      playList={this.state.playList}
-      movieList={this.state.movieList}
+      playList={this.props.playList}
+      movieList={this.props.movieList}
     />
   </div>
 );
